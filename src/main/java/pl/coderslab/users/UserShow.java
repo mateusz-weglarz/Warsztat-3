10ccf6f94950650becc0014a7e55d44c63330eed
mateusz-weglarz <mjw1992@interia.pl>
2021-02-06 11:12:59 +0100

    Warsztat 3, userShow działa

--- src/main/java/pl/coderslab/users/UserShow.java
+++ src/main/java/pl/coderslab/users/UserShow.java
@@ -1,5 +1,7 @@
 package pl.coderslab.users;
 
+import pl.coderslab.Dao.UserDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,7 +11,10 @@ import java.io.IOException;
 public class UserShow extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        UserDao userDao = new UserDao();
+        int id = Integer.parseInt(request.getParameter("id"));
+        request.setAttribute("user", userDao.readId(id));
+        getServletContext().getRequestDispatcher("/user/userShow.jsp").forward(request, response);
     }
 
     @Override
